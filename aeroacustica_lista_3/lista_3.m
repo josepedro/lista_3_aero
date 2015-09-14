69f55db6da272b3281b7eea055d53b86355838a4
Jose Pedro <1jpsneto@gmail.com>
2015-09-14 20:06:39 -0300

    1

--- aeroacustica_lista_3/lista_3.m
+++ aeroacustica_lista_3/lista_3.m
@@ -8,6 +8,8 @@ velocidades = open('velocidades.mat');
 velocidades_x = velocidades.vel_x;
 velocidades_y = velocidades.vel_y;
 velocidades_z = velocidades.vel_z;
+rho = 1.2; % kg/m^3
+delta_x = 0.003;
 
 figure;
 hold on;
@@ -19,10 +21,49 @@ plot3(velocidades_z(:,1), velocidades_z(:,2), velocidades_z(:,3), 'g');
 
 % Obs: Não há nenhuma velocidade em Z
 
-% Construindo matriz espacial de 3 dimensões
+% Multiplicando as 3 matrizes, construindo vivj
+disp('Multiplicando as matrizes');
+% Para velocidades em x
+magnitude_velocidade_x = velocidades_x.^2;
+% Para velocidades em y
+magnitude_velocidade_y = velocidades_y.^2;
 
+% Calculando o rho*vi*vj
+disp('Calculando o rho*vi*vj');
+rho_vi_vj = rho*(sqrt(magnitude_velocidade_x + magnitude_velocidade_y));
 
-% Realizando o calculo da velocidade X
-pressao_sonora_x = 
+% Dividindo pela magnitude do espaco e 4*pi (|x|)
+matriz_x = [1:100]*delta_x;
+matriz_y = [1:95]*delta_x;
+matriz_z = [1:100]*delta_x;
 
-sum(velocidades_z(:,1)) + sum(velocidades_z(:,2)) + sum(velocidades_z(:,3))
+disp('Dividindo pela magnitude do espaco e 4*pi (|x|)');
+tamanhos = 	size(rho_vi_vj);
+i = 0;
+matriz_distancia_i_j_k = {};
+for	x_i = 1:tamanhos(1)
+	for	x_j = 1:tamanhos(2)
+		for x_k = 1:tamanhos(3)
+			disp('x_k');
+			matriz_distancia{x_i*x_j*x_k} = [x_i - 1, x_j - 1, x_k - 1]*0.03;
+			i = i + 1;
+			disp(i);
+		end
+	end
+end
+
+for ponto = 1:tamanhos(1)*tamanhos(2)*tamanhos(3)
+	rho_vi_vj	
+end
+
+%rho_vi_vj = rho_vi_vj/4*pi*(sqrt(x_i^2 + x_j^2 + x_k^2));
+
+% Calculando o laplaciano para depois integrar
+disp('Calculando o laplaciano para depois integrar');
+%laplaciano_rho_vi_vj = 	diff(diff(rho_vi_vj));
+
+% Calculando a pressao final
+disp('Calculando a pressao final');
+%pressao = trapz(laplaciano_rho_vi_vj, ndims(laplaciano_rho_vi_vj));
+
+%pressao(15,15,15)
