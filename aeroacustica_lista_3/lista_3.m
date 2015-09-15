d2114733c7fca534de469a6745b12822e164aa22
Jose Pedro <1jpsneto@gmail.com>
2015-09-15 00:08:05 -0300

    2

--- aeroacustica_lista_3/lista_3.m
+++ aeroacustica_lista_3/lista_3.m
@@ -41,29 +41,39 @@ disp('Dividindo pela magnitude do espaco e 4*pi (|x|)');
 tamanhos = 	size(rho_vi_vj);
 i = 0;
 matriz_distancia_i_j_k = {};
+estrutura_rho_vi_vj = {};
+
 for	x_i = 1:tamanhos(1)
 	for	x_j = 1:tamanhos(2)
 		for x_k = 1:tamanhos(3)
-			disp('x_k');
+			estrutura_rho_vi_vj{x_i*x_j*x_k} = rho_vi_vj(x_i, x_j, x_k);
 			matriz_distancia{x_i*x_j*x_k} = [x_i - 1, x_j - 1, x_k - 1]*0.03;
 			i = i + 1;
-			disp(i);
 		end
 	end
 end
 
+distancia = 0;
+derivado_rho_vi_vj(1:tamanhos(1)*tamanhos(2)*tamanhos(3)) = 0;
+disp('Calculando a bagaceira');
 for ponto = 1:tamanhos(1)*tamanhos(2)*tamanhos(3)
-	rho_vi_vj	
+	subtracao = matriz_distancia{1}-[15 15 15];
+	elevando_quadrado = subtracao.^2;
+	soma = sum(elevando_quadrado);
+	distancia = sqrt(soma);
+	derivado_rho_vi_vj(ponto) = estrutura_rho_vi_vj{ponto}/distancia;
 end
+
+
 
 %rho_vi_vj = rho_vi_vj/4*pi*(sqrt(x_i^2 + x_j^2 + x_k^2));
 
 % Calculando o laplaciano para depois integrar
 disp('Calculando o laplaciano para depois integrar');
-%laplaciano_rho_vi_vj = 	diff(diff(rho_vi_vj));
+laplaciano_rho_vi_vj = 	diff(diff(derivado_rho_vi_vj));
 
 % Calculando a pressao final
 disp('Calculando a pressao final');
-%pressao = trapz(laplaciano_rho_vi_vj, ndims(laplaciano_rho_vi_vj));
+pressao = trapz(laplaciano_rho_vi_vj, ndims(laplaciano_rho_vi_vj));
 
 %pressao(15,15,15)
