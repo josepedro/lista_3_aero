70b1815ade1911accbc0909a19c0e010760d5547
Jose Pedro <1jpsneto@gmail.com>
2015-09-15 01:54:00 -0300

    3

--- aeroacustica_lista_3/lista_3.m
+++ aeroacustica_lista_3/lista_3.m
@@ -11,69 +11,33 @@ velocidades_z = velocidades.vel_z;
 rho = 1.2; % kg/m^3
 delta_x = 0.003;
 
-figure;
-hold on;
-grid on;
-grid minor;
-plot3(velocidades_x(:,1), velocidades_x(:,2), velocidades_x(:,3), 'b');
-plot3(velocidades_y(:,1), velocidades_y(:,2), velocidades_y(:,3), 'r');
-plot3(velocidades_z(:,1), velocidades_z(:,2), velocidades_z(:,3), 'g');
-
-% Obs: Não há nenhuma velocidade em Z
-
-% Multiplicando as 3 matrizes, construindo vivj
-disp('Multiplicando as matrizes');
-% Para velocidades em x
-magnitude_velocidade_x = velocidades_x.^2;
-% Para velocidades em y
-magnitude_velocidade_y = velocidades_y.^2;
-
 % Calculando o rho*vi*vj
-disp('Calculando o rho*vi*vj');
-rho_vi_vj = rho*(sqrt(magnitude_velocidade_x + magnitude_velocidade_y));
+magnitude_velocidade_x = velocidades_x(:,:,1).^2;
+magnitude_velocidade_y = velocidades_y(:,:,1).^2;
+rho_vi_vj = rho*(sqrt(sum(magnitude_velocidade_x + magnitude_velocidade_y)));
 
-% Dividindo pela magnitude do espaco e 4*pi (|x|)
-matriz_x = [1:100]*delta_x;
-matriz_y = [1:95]*delta_x;
-matriz_z = [1:100]*delta_x;
-
-disp('Dividindo pela magnitude do espaco e 4*pi (|x|)');
-tamanhos = 	size(rho_vi_vj);
-i = 0;
-matriz_distancia_i_j_k = {};
-estrutura_rho_vi_vj = {};
-
-for	x_i = 1:tamanhos(1)
-	for	x_j = 1:tamanhos(2)
-		for x_k = 1:tamanhos(3)
-			estrutura_rho_vi_vj{x_i*x_j*x_k} = rho_vi_vj(x_i, x_j, x_k);
-			matriz_distancia{x_i*x_j*x_k} = [x_i - 1, x_j - 1, x_k - 1]*0.03;
-			i = i + 1;
-		end
+% Dividindo pelo raio
+tamanhos = size(rho_vi_vj);
+for x_i = 1:tamanhos(1)
+	for x_j = 1:tamanhos(2)
+		%distancia =  sqrt((x_i*0.003)^2 + (x_j*0.003)^2);
+		distancia =  sqrt((15)^2 + (15)^2);
+		rho_vi_vj(x_i, x_j) = rho_vi_vj(x_i, x_j)/4*pi*distancia;
 	end
 end
 
-distancia = 0;
-derivado_rho_vi_vj(1:tamanhos(1)*tamanhos(2)*tamanhos(3)) = 0;
-disp('Calculando a bagaceira');
-for ponto = 1:tamanhos(1)*tamanhos(2)*tamanhos(3)
-	subtracao = matriz_distancia{1}-[15 15 15];
-	elevando_quadrado = subtracao.^2;
-	soma = sum(elevando_quadrado);
-	distancia = sqrt(soma);
-	derivado_rho_vi_vj(ponto) = estrutura_rho_vi_vj{ponto}/distancia;
-end
-
-
-
-%rho_vi_vj = rho_vi_vj/4*pi*(sqrt(x_i^2 + x_j^2 + x_k^2));
-
 % Calculando o laplaciano para depois integrar
-disp('Calculando o laplaciano para depois integrar');
-laplaciano_rho_vi_vj = 	diff(diff(derivado_rho_vi_vj));
+laplaciano_rho_vi_vj = 	del2(rho_vi_vj);
+%surf(laplaciano_rho_vi_vj)
 
 % Calculando a pressao final
-disp('Calculando a pressao final');
-pressao = trapz(laplaciano_rho_vi_vj, ndims(laplaciano_rho_vi_vj));
+pressao = trapz(laplaciano_rho_vi_vj);
 
-%pressao(15,15,15)
+
+%figure;
+%hold on;
+%grid on;
+%grid minor;
+%plot3(velocidades_x(:,1), velocidades_x(:,2), velocidades_x(:,3), 'b');
+%plot3(velocidades_y(:,1), velocidades_y(:,2), velocidades_y(:,3), 'r');
+%plot3(velocidades_z(:,1), velocidades_z(:,2), velocidades_z(:,3), 'g');
