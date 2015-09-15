cedf461ea31fce16420cddfdbc4e63a053671acc
Jose Pedro <1jpsneto@gmail.com>
2015-09-15 13:43:25 -0300

    5

--- aeroacustica_lista_3/lista_3.m
+++ aeroacustica_lista_3/lista_3.m
@@ -3,7 +3,7 @@ close all;
 
 % Lista de Exercícios 3
 
-% Plotando as velocidades em 3D
+disp('Questão 1 ----------------------');
 velocidades = open('velocidades.mat');
 velocidades_x = velocidades.vel_x;
 velocidades_y = velocidades.vel_y;
@@ -15,8 +15,6 @@ delta_x = 0.003;
 magnitude_velocidade_x = velocidades_x(:,:,1).^2;
 magnitude_velocidade_y = velocidades_y(:,:,1).^2;
 rho_vi_vj = rho*(sqrt(sum(magnitude_velocidade_x + magnitude_velocidade_y)));
-figure;
-plot(rho_vi_vj);
 
 % Dividindo pelo raio
 tamanhos = size(rho_vi_vj);
@@ -27,8 +25,6 @@ for x_i = 1:tamanhos(1)
 		rho_vi_vj(x_i, x_j) = rho_vi_vj(x_i, x_j)/4*pi*distancia;
 	end
 end
-figure;
-plot(rho_vi_vj);
 
 % Calculando o laplaciano para depois integrar
 rho_vi_vj_a(1:length(rho_vi_vj)) = 0;
@@ -41,14 +37,33 @@ for n = 1:length(rho_vi_vj_a)-2
 	rho_vi_vj_a(n) = (rho_vi_vj(n+1)-rho_vi_vj(n))/delta_x;
 end
 laplaciano_rho_vi_vj = rho_vi_vj_a(1:length(rho_vi_vj)-2);
-%surf(laplaciano_rho_vi_vj)
-figure;
-plot(laplaciano_rho_vi_vj);
 
 % Calculando a pressao final
-pressao = trapz(laplaciano_rho_vi_vj);
+pressao_acustica = trapz(laplaciano_rho_vi_vj);
+resposta = ['Valor de Pressão Acústica: ', num2str(pressao_acustica, '%10.5e'), ...
+' 	N/m^2'];
+disp(resposta);
 
+disp('Questão 2 ----------------------');
+% Plotando mapa de superfície de velocidade absoluta
+velocidades_x = velocidades_x(:,:,1);
+velocidades_y = velocidades_y(:,:,1);
+velocidades_absolutas = sqrt(velocidades_x.^2 + velocidades_y.^2);
+[x,y] = meshgrid([0:99]*0.003);
+x = x(:, 1:95);
+y = y(:, 1:95);
+figure;
+surf(velocidades_absolutas);
+vorticidade = curl(velocidades_x, velocidades_y);
+figure;
+surf(x, y, vorticidade);
+resposta = ['O valor da dimensao característica é 0.0630 metros ou 63 mm', ...
+' pois a turbulencia possui um centro definido com esse diâmetro característico.'];
+disp(resposta);	
 
+disp('Questão 3 ----------------------');
+
+% Plotando mapa de superfície de velocidade absoluta
 %figure;
 %hold on;
 %grid on;
