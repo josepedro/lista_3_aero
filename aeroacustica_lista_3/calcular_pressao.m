b115c13e7acce7f81c8e6a535365942389615264
Jose Pedro <1jpsneto@gmail.com>
2015-09-19 16:50:57 -0300

    question 1 done

--- aeroacustica_lista_3/calcular_pressao.m
+++ aeroacustica_lista_3/calcular_pressao.m
@@ -1,30 +1,47 @@
-%% functionname: function description
-function pressao_acustica = calcular_pressao(rho, delta_x, velocidades_x, velocidades_y, coordenadas_x)
+function pressao_acustica = calcular_pressao(rho, delta_x, velocidades_x, ...
+	velocidades_y, posicao_ouvinte, matriz_cubica)
 
+	% Calculando o vi*vj
+	vi_vj = sqrt(sum(sum(velocidades_x.^2)) + sum(sum(velocidades_y.^2)));
 	% Calculando o rho*vi*vj
-	rho_vi_vj = rho*(sqrt(sum(velocidades_x.^2 + velocidades_y.^2)));
-	
-	% Dividindo pelo raio
-	tamanhos = size(rho_vi_vj);
-	for x_i = 1:tamanhos(1)
-		for x_j = 1:tamanhos(2)
-			%distancia =  sqrt((x_i*0.003)^2 + (x_j*0.003)^2);
-			distancia =  sqrt((coordenadas_x(1))^2 + (coordenadas_x(2))^2);
-			rho_vi_vj(x_i, x_j) = rho_vi_vj(x_i, x_j)/4*pi*distancia;
+	rho_vi_vj = rho*vi_vj;
+
+	% Preenchendo a matriz pelo escalar rho_vi_vj
+	matriz_tensor_lighthill = matriz_cubica;
+	matriz_tensor_lighthill(:) = 1;
+	matriz_tensor_lighthill = matriz_tensor_lighthill*rho_vi_vj;
+
+	% Calculando a distancia |x - y|
+	tamanhos = size(matriz_cubica);
+	for x = 1:tamanhos(1)
+		for y = 1:tamanhos(2)
+			for z = 1:tamanhos(3)
+				posicao_x = posicao_ouvinte(1) - x*0.003;
+				posicao_y = posicao_ouvinte(2) - y*0.003;
+				posicao_z = posicao_ouvinte(3) - z*0.003;
+				distancia = sqrt(posicao_x^2 + posicao_y^2 + posicao_z^2);
+				A = matriz_tensor_lighthill(x, y, z);
+				B =  4*pi*distancia;
+				B = 1/B;
+				matriz_tensor_lighthill(x, y, z) = A*B;
+			end
 		end
 	end
 
 	% Calculando o laplaciano para depois integrar
-	rho_vi_vj_a(1:length(rho_vi_vj)) = 0;
-	% derivando a primeira vez
-	for n = 1:length(rho_vi_vj_a)-1
-		rho_vi_vj_a(n) = (rho_vi_vj(n+1)-rho_vi_vj(n))/delta_x;
-	end
-	% derivando a segunda vez
-	for n = 1:length(rho_vi_vj_a)-2
-		rho_vi_vj_a(n) = (rho_vi_vj(n+1)-rho_vi_vj(n))/delta_x;
-	end
-	laplaciano_rho_vi_vj = rho_vi_vj_a(1:length(rho_vi_vj)-2);
+	%size(matriz_tensor_lighthill)
+
+	diferenciado_x_tensor_lighthill = diff(matriz_tensor_lighthill, 2, 1);
+	diferenciado_x_tensor_lighthill = diferenciado_x_tensor_lighthill;
+	diferenciado_xy_tensor_lighthill = diff(diferenciado_x_tensor_lighthill, 2, 2);
+	diferenciado_xy_tensor_lighthill = diferenciado_xy_tensor_lighthill;
+	diferenciado_xyz_tensor_lighthill = diff(diferenciado_xy_tensor_lighthill, 2, 3);
+	laplaciano_tensor_lighthill = diferenciado_xyz_tensor_lighthill;
+								
+	%size(laplaciano_tensor_lighthill)
+	%laplaciano_tensor_lighthill(1,1,:)
 
 	% Calculando a pressao final
-	pressao_acustica = trapz(laplaciano_rho_vi_vj);
+	pressao_acustica_x = trapz(laplaciano_tensor_lighthill,1);
+	pressao_acustica_xy = trapz(pressao_acustica_x,2);
+	pressao_acustica = trapz(pressao_acustica_xy,3);
--- aeroacustica_lista_3/lista_3.m
+++ aeroacustica_lista_3/lista_3.m
@@ -9,51 +9,9 @@ velocidades_x = velocidades.vel_x(:,:,1);
 velocidades_y = velocidades.vel_y(:,:,1);
 rho = 1.2; % kg/m^3
 delta_x = 0.003; % m
-coordenadas_x = [15 15]; % m
+posicao_ouvinte = [15 15 15]; % m
 
+pressao_acustica = calcular_pressao(rho, delta_x, velocidades_x, velocidades_y, posicao_ouvinte, velocidades.vel_x);
 
-
-pressao_acustica = calcular_pressao(rho, delta_x, velocidades_x, velocidades_y, coordenadas_x);
-
-resposta = ['Valor de Pressão Acústica: ', num2str(pressao_acustica, '%10.5e'), ...
-' 	N/m^2'];
-disp(resposta);
-
-disp('Questão 2 ----------------------');
-% Plotando mapa de superfície de velocidade absoluta
-velocidades_absolutas = sqrt(velocidades_x.^2 + velocidades_y.^2);
-[x,y] = meshgrid([0:99]*0.003);
-x = x(:, 1:95);
-y = y(:, 1:95);
-figure;
-surf(velocidades_absolutas);
-vorticidade = curl(velocidades_x, velocidades_y);
-figure;
-surf(x, y, vorticidade);
-resposta = ['O valor da dimensao característica é 0.0630 metros ou 63 mm', ...
-' pois a turbulencia possui um centro definido com esse diâmetro característico.'];
-disp(resposta);	
-
-disp('Questão 3 ----------------------');
-% Plotando grafico de pressao por velocidades atraves da equacao 1
-pressao_velocidades_1(1:10) = 0;
-pressao_velocidades_2(1:10) = 0;
-for divisao = 1:10
-	velocidade_divisao = 10^divisao;
-	pressao_velocidades_1(divisao) = calcular_pressao(rho, delta_x, velocidades_x/velocidade_divisao ...
-	, velocidades_y/velocidade_divisao, coordenadas_x);
-	pressao_velocidades_2(divisao) = 
-end
-figure;
-
-plot(pressao_velocidades_1);
-
-
-% Plotando mapa de superfície de velocidade absoluta
-%figure;
-%hold on;
-%grid on;
-%grid minor;
-%plot3(velocidades_x(:,1), velocidades_x(:,2), velocidades_x(:,3), 'b');
-%plot3(velocidades_y(:,1), velocidades_y(:,2), velocidades_y(:,3), 'r');
-%plot3(velocidades_z(:,1), velocidades_z(:,2), velocidades_z(:,3), 'g');
+valor_referencia = 2*10^-5;
+nivel_pressao_sonora_dB = 20*log((pressao_acustica+valor_referencia)/valor_referencia);
